a96e8b0ddd1f8c04ee3585acc07dba33a5831bc5
daniel.pessine <daniel.pessine@r210-222.schule.local>
2019-03-05 12:20:53 +0100

    Anagram

--- Einfuehrung/src/Anagram.java
+++ Einfuehrung/src/Anagram.java
@@ -1,11 +1,47 @@
+import java.util.Arrays;
 
 public class Anagram {
-
-	public static void main(String[] args) 
-	{
 	
+	public static void main(String[] args) {
+		
+		
+		
+		String str1 = "String one";
+		String str2 = "String one";
+		
+		str1.toCharArray();
+		str2.toCharArray();
+
+		sindAnagramme(str1, str2);
+		
+		if(sindAnagramme(str1, str2))
+		{
+			System.out.println("Die gegebenen Strings sind Anagramme");
+		}
+	}
+
+static boolean sindAnagramme(char [] str1, char [] str2)
+{
+	int n1 = str1.length;
+	int n2 = str2.length;
+	
+	if( n1 != n2 )
+	{
+		return false;
 	}
 	
-
-
+	Arrays.sort(str1);
+	Arrays.sort(str2);
+	
+	for( int i = 0; i < n2; i++)
+	{
+		if(str1[i] != str2[i])
+		{
+			return false;
+		}
+	}
+	
+	return true;
+}
+	
 }
