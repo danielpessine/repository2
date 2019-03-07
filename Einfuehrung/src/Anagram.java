8537898330569f9b2554f017fe6990fab64a6059
daniel.pessine <daniel.pessine@r210-222.schule.local>
2019-03-07 10:05:42 +0100

    Anagram

--- Einfuehrung/src/Anagram.java
+++ Einfuehrung/src/Anagram.java
@@ -7,35 +7,35 @@ public class Anagram {
 		
 		
 		String str1 = "String one";
-		String str2 = "String one";
+		String str2 = "String owe";
 		
 		char [] str11 = str1.toCharArray();
-		char str22 []= str2.toCharArray();
+		char [] str22 = str2.toCharArray();
 
-		sindAnagramme(str1, str2);
+		sindAnagramme(str11, str22);
 		
-		if(sindAnagramme(str1, str2))
+		if(sindAnagramme(str11, str22))
 		{
-			System.out.println("Die gegebenen Strings sind Anagramme");
+			System.out.println("Die gegebenen Strings sind Anagramme!");
 		}
 	}
 
-static boolean sindAnagramme(char [] str1, char [] str2)
+static boolean sindAnagramme(char [] str11, char [] str22)
 {
-	int n1 = str1.length;
-	int n2 = str2.length;
+	int n1 = str11.length;
+	int n2 = str22.length;
 	
 	if( n1 != n2 )
 	{
 		return false;
 	}
 	
-	Arrays.sort(str1);
-	Arrays.sort(str2);
+	Arrays.sort(str11);
+	Arrays.sort(str22);
 	
 	for( int i = 0; i < n2; i++)
 	{
-		if(str1[i] != str2[i])
+		if(str11[i] != str22[i])
 		{
 			return false;
 		}
